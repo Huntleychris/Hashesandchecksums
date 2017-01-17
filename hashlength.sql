1746b5ead372031ff4e55d752c0218559ced3152
Chris Huntley <chuntley@blackshellconsulting.com>
2017-01-16 21:03:03 -0600

    just adding some comments

--- hashlength.sql
+++ hashlength.sql
@@ -1,8 +1,11 @@
-SELECT  DATALENGTH(HASHBYTES('MD2', 'Testing')) AS [MD2Length],
+SELECT  
+-- showing the possible hashbytes options by length so you can choose the approprite binary length
+		DATALENGTH(HASHBYTES('MD2', 'Testing')) AS [MD2Length],
         DATALENGTH(HASHBYTES('MD4', 'Testing')) AS [MD4Length],
+		-- probably shouldn't use less than MD5
         DATALENGTH(HASHBYTES('MD5', 'Testing')) AS [MD5Length],
         DATALENGTH(HASHBYTES('SHA', 'Testing')) AS [SHALength],
         DATALENGTH(HASHBYTES('SHA1', 'Testing')) AS [SHA1Length],
-        /* 2012 only: */
+		-- I'm hashing keys with this output seems to be good at avoiding a collision
         DATALENGTH(HASHBYTES('SHA2_256', 'Testing')) AS [SHA2_256Length],
         DATALENGTH(HASHBYTES('SHA2_512', 'Testing')) AS [SHA2_512Length];
